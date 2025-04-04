5eb253599a45007bc84c18a7d6a292688d48707b
Mkeller777 <mkeller@allpointretial.com>
2025-04-04 15:20:51 -0400

    adding function to selecta random prefix and a function to build the random message

--- main.js
+++ main.js
@@ -2,6 +2,19 @@ function selectRandomQuote() {
     const quotesList = require('./quotes.json').quotes;
     const randomIndex = Math.floor(Math.random() * quotesList.length);
     const quoteObject = quotesList[randomIndex];
-    return `${quoteObject.message} -${quoteObject.author}.`
+    return `"${quoteObject.message}" -${quoteObject.author}`
 }
-console.log(selectRandomQuote());
+
+function selectRandomPrefix() {
+    const prefixList = require('./quotes.json').prefixes;
+    const randomIndex = Math.floor(Math.random() * prefixList.length);
+    return prefixList[randomIndex].prompt;
+}
+
+function buildMessage() {
+    const prefix = selectRandomPrefix();
+    const quote = selectRandomQuote();
+    return `${prefix} ${quote}`;
+}
+
+console.log(buildMessage());
